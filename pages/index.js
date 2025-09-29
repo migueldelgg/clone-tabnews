b4b61362c20763ab50ec156513fc3183862ba202
Miguel Delgado <150628051+migueldelgg@users.noreply.github.com>
2025-09-29 15:13:52 +0000

    feat(pages/index.js): adiciona pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 function Home() {
   return (
-    <div>
-      <h1>Hemilly, te amo.</h1>
-      <span>se me amas, de uma risadinha! 💕</span>
+    <div
+      style={{
+        color: "black",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "'Lobster', cursive",
+        textAlign: "center",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <h1>This site is under construction, come back later.</h1>
+        <span>
+          You can connect with me through
+          <a
+            href="https://www.linkedin.com/in/delgadomiguel/"
+            style={{
+              color: "#0077B5",
+              textDecoration: "none",
+              marginLeft: "5px",
+            }}
+          >
+            Linkedin
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
